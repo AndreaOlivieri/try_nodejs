f8eaa12d4c5099f50909e1ade61f81be25b9005f
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2017-04-22 22:16:57 +0200

    added redis

--- app.js
+++ app.js
@@ -6,24 +6,28 @@ var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
-var cities = {
-  'Lotopia': 'description',
-  'Caspiana': 'description',
-  'Indigo': 'description'
-};
+var redis = require('redis');
+var client = redis.createClient();
+
+client.select(process.env.NODE_ENV.length || 'development'.length);
 
 app.get('/', function(request, response){
   response.send('OK');
 });
 
 app.get('/cities', function(request, response){
-  response.json(Object.keys(cities));
+  client.hkeys('cities', function(error, names){
+    if (error) throw error;
+    response.json(names);
+  })
 });
 
 app.post('/cities', urlencode, function(request, response){
   var newCity = request.body;
-  cities[newCity.name] = newCity.description;
-  response.status(201).json(newCity.name);
+  client.hset('cities', newCity.name, newCity.description, function(error, name){
+    if (error) throw error;
+    response.status(201).json(newCity.name);
+  });
 });
 
 module.exports = app;
